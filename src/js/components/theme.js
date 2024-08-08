031e4522d46e9290682051e7c6f05f967aa68528
Test Name <testmail@mail.ru>
2024-08-08 13:07:20 +0300

    created header

---
+++ src/js/components/theme.js
@@ -0,0 +1,25 @@
+// реализация переключения темы
+
+export const useTheme = () => {
+  const themeTarget = document.querySelector('[data-theme]');
+  const themeSwitcher = document.querySelector('[data-theme-switcher]');
+
+  const theme = localStorage.getItem('theme') || 'light';
+  themeTarget.dataset.theme = theme;
+
+  if (theme === 'dark') {
+    themeSwitcher.checked = true;
+  } else {
+    themeSwitcher.checked = false;
+  }
+
+  themeSwitcher.addEventListener('click', () => {
+    if (themeTarget.dataset.theme === 'light') {
+      themeTarget.dataset.theme = 'dark';
+      localStorage.setItem('theme', 'dark');
+    } else {
+      themeTarget.dataset.theme = 'light';
+      localStorage.setItem('theme', 'light');
+    }
+  });
+};
