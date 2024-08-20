4e3d40dbedc5573bedba3fb2325a10581011d996
Test Name <testmail@mail.ru>
2024-08-20 21:20:44 +0300

    Added and styled insight section

---
+++ src/js/components/slider.js
@@ -0,0 +1,17 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
