77f927018f7a0e3c2ca6dbad85c429f582dd2203
Test Name <testmail@mail.ru>
2024-10-13 12:26:22 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   root: './src',
-  base: './coca/',
+  base: '/coca/',
   server: {
     port: 3000,
     open: true,
